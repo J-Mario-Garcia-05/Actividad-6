8db2730667a30586b7ed795cffbdac871d3545d9
J-Mario-García <jm.gcaxaj@udeo.edu.gt>
2025-07-14 11:31:03 -0600

    Guardar en diccionario

--- Actividad_6.py
+++ Actividad_6.py
@@ -4,10 +4,16 @@ for i in range(cantidad):
     print(f"Productio: {i + 1}")
     codigo = input("Código del producto: ")
     nombre = input("Nombre del producto: ")
-    categoria = input("Categoría del producto: ")
+    categoria = input("Categoría del producto (hombre/mujer): ")
     talla = input("Talla del producto: ")
     precio = input("Precio del producto: ")
     if precio < 0:
         print("Precio no valido, tien que ser mayor a 0")
         continue
     cantidad = int(input("Ingrese la cantidad del producto en stock: "))
+    inventario[codigo] = {
+        "nombre": nombre,
+        "categoria": categoria,
+        "precio": precio,
+        "talla": talla,
+    }
