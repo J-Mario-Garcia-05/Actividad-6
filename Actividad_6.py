c0b402ca223db34cd84d4d30c7e64dbe5027035c
J-Mario-García <jm.gcaxaj@udeo.edu.gt>
2025-07-14 20:02:42 -0600

    Agregar validaciones

--- Actividad_6.py
+++ Actividad_6.py
@@ -3,14 +3,19 @@ cantidad = int(input("Ingrese la cantidad de productos que va a registrar: "))
 for i in range(cantidad):
     print(f"Productio: {i + 1}")
     codigo = input("Código del producto: ")
+    if codigo in inventario:
+        print("El codigo que ingreso ya se encuentra registrado en otro producto.")
+        continue
     nombre = input("Nombre del producto: ")
     categoria = input("Categoría del producto (hombre/mujer): ")
     talla = input("Talla del producto: ")
-    precio = input("Precio del producto: ")
+    precio = int(input("Precio del producto: Q."))
     if precio < 0:
         print("Precio no valido, tien que ser mayor a 0")
         continue
     cantidad = int(input("Ingrese la cantidad del producto en stock: "))
+    if cantidad < 0:
+        print("No puede ingresar una cantidad menor a 0.")
     inventario[codigo] = {
         "nombre": nombre,
         "categoria": categoria,
