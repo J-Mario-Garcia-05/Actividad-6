f624ae32512bd334e71bd82653fdfbc42ab0c998
J-Mario-García <jm.gcaxaj@udeo.edu.gt>
2025-07-14 21:18:42 -0600

    Version de entrega

--- Actividad_6.py
+++ Actividad_6.py
@@ -12,14 +12,22 @@ while i <= cantidad:
     if categoria != "hombre" and categoria != "mujer" and categoria != "niño":
         print("Categoría no válida, repita nuevamente el registro.")
         continue
-    talla = input("Talla del producto: ")
-    precio = int(input("Precio del producto: Q."))
-    if precio < 0:
-        print("Precio no valido, tien que ser mayor a 0, repita nuevamente el registro.")
+    try:
+        talla = int(input("Talla del producto: "))
+        if talla < 0:
+            print("Talla ingresada no válida, repita nuevamente el registro.")
+            continue
+        precio = int(input("Precio del producto: Q."))
+        if precio < 0:
+            print("Precio no valido, tien que ser mayor a 0, repita nuevamente el registro.")
+            continue
+        stock = int(input("Ingrese la cantidad del producto en stock: "))
+        if stock < 0:
+            print("No puede ingresar una cantidad menor a 0, repita nuevamente el registro.")
+            continue
+    except Exception as e:
+        print("Ha ocurrido un error en el registro, " + str(e))
         continue
-    stock = int(input("Ingrese la cantidad del producto en stock: "))
-    if stock < 0:
-        print("No puede ingresar una cantidad menor a 0.")
     inventario[codigo] = {
         "nombre": nombre,
         "categoria": categoria,
@@ -28,33 +36,36 @@ while i <= cantidad:
         "stock": stock
     }
     i += 1
+input("Presione ENTER para continuar")
 
-print("Productos registrados:")
+print("\nProductos registrados:")
 for codigo, producto in inventario.items():
     print(f"\nCódigo del producto: {codigo}")
     print(f"Nombre del producto: {producto['nombre']}\tCategoria: {producto['categoria']}\tTalla: {producto['talla']}")
-    print(f"Precio del producto: {producto['precio']}\tCantidad en stock: {producto['stock']}")
+    print(f"Precio del producto: Q.{producto['precio']}\tCantidad en stock: {producto['stock']}")
 
-buscar = input("Ingrese el códgio del producto que desea buscar: ")
+buscar = input("\nIngrese el código del producto que desea buscar: ")
 if buscar in inventario:
     print(f"Nombre del producto: {inventario[buscar]['nombre']}\tCategoria: {inventario[buscar]['categoria']}")
-    print(f"Talla: {inventario[buscar]['talla']}\tPrecio del producto: {producto['precio']}")
-    print(f"Cantidad en stock: {producto['stock']}")
+    print(f"Talla: {inventario[buscar]['talla']}\tPrecio del producto: Q.{inventario[buscar]['precio']}")
+    print(f"Cantidad en stock: {inventario[buscar]['stock']}")
+    valor_total = inventario[buscar]["precio"] * inventario[buscar]["stock"]
+    print(f"Valor total del producto en inventario: Q.{valor_total:.2f}")
+else:
+    print("No existe el producto en inventario.")
+input("Presione ENTER para continuar")
 
-    valor_total = producto["precio"] * producto["stock"]
-    print(f"Valor total del producto en inventario: {valor_total}")
-
-print("Cantidad por categoría:")
+print("\nCantidad por categoría:")
 cantidad_producto = 0
 producto_mujer = 0
 producto_hombre = 0
 for codigo, producto in inventario.items():
     if producto['categoria'] == "mujer":
-        producto_mujer += 1
+        producto_mujer += producto['stock']
     elif producto['categoria'] == "hombre":
-        producto_hombre += 1
+        producto_hombre += producto['stock']
     else:
-        cantidad_producto += 1
+        cantidad_producto += producto['stock']
 print(f"Productos de niño: {cantidad_producto}")
 print(f"Productos de mujer: {producto_mujer}")
 print(f"Productos de hombre: {producto_hombre}")
